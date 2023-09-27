d2b527ea384a8018814a763507fb35a7b44052cc
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2023-09-27 13:26:15 +0530

    add dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,21 @@
 import Scaffold from '@/components/Scaffold'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import {Button, ButtonGroup, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem} from "@nextui-org/react";
+
 
 export default function Home() {
   return (
     <Scaffold>
-      <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+      {/* <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" /> */}
       <div className="text-center lg:w-2/3 w-full">
         <motion.h1 initial={{opacity: 0, y:20}} whileInView={{ opacity: 1, y: 0 }} transition={{ type: "spring", duration: 0.8 }} className='text-8xl font-semibold mb-6'>Blockbase</motion.h1>
         <motion.h2 initial={{opacity: 0, y:20}} whileInView={{ opacity: 1, y: 0 }} transition={{ type: "spring", duration: 0.8, delay: 0.4 }} className='text-4xl '>Secure E-Vault for all your needs.</motion.h2>            <div className="flex justify-center">
-          <Button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-          <Button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+        
+        <motion.div initial={{opacity: 0, y:20}} whileInView={{ opacity: 1, y: 0 }} transition={{ type: "spring", duration: 0.8, delay: 0.6 }} className='mt-8'>
+          <Button className='mx-2'>Explore Chains</Button>
+          <Button className='mx-2'>Learn More</Button>
+        </motion.div>
         </div>
       </div>
     </Scaffold>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,48 +1,114 @@
 "use client"
 
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/navbar";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu } from "@nextui-org/react";
 import { Button } from "@nextui-org/button";
 import { motion } from "framer-motion";
 import { Link } from "@nextui-org/link"
+import {ChevronDown, Document, Activity, Flash, Server, TagUser, Scale} from "@/components/Icons";
 
 export default function Header() {
+	const icons = {
+		chevron: <ChevronDown fill="currentColor" size={16} />,
+		scale: <Scale className="text-warning" fill="currentColor" size={30} />,
+		document: <Document className="text-success" fill="currentColor" size={30} />,
+		activity: <Activity className="text-secondary" fill="currentColor" size={30} />,
+		flash: <Flash className="text-primary" fill="currentColor" size={30} />,
+		server: <Server className="text-success" fill="currentColor" size={30} />,
+		user: <TagUser className="text-danger" fill="currentColor" size={30} />,
+	  };
+
 	return (
-		<motion.div initial={{opacity: 0, y:-20}} whileInView={{ opacity: 1, y: 0 }} transition={{ type: "spring", duration: 0.8, delay: 0.2 }} className="w-full">
-		<Navbar shouldHideOnScroll maxWidth="xl">
-			<NavbarBrand>
-				<p className="font-bold text-inherit">Blockbase</p>
-			</NavbarBrand>
-			<NavbarContent className="hidden sm:flex gap-4" justify="center">
-				<NavbarItem>
-					<Link underline="hover" color="foreground" href="#">
-						About
-					</Link>
-				</NavbarItem>
-				<NavbarItem>
-					<Link underline="hover" color="foreground" href="#">
-						Customers
-					</Link>
-				</NavbarItem>
-				<NavbarItem>
-					<Link underline="hover" color="foreground" href="#">
-						Contact
-					</Link>
-				</NavbarItem>
-			</NavbarContent>
-			<NavbarContent justify="end">
-				<NavbarItem>
-					<Button as={Link} color="primary" href="#" variant="flat">
-						Citizen Login
-					</Button>
-				</NavbarItem>
-				<NavbarItem>
-					<Button as={Link} color="primary" href="#" variant="flat">
-						Official Login
-					</Button>
-				</NavbarItem>
-			</NavbarContent>
-		</Navbar>
+		<motion.div initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ type: "spring", duration: 0.8, delay: 0.2 }} className="w-full">
+			<Navbar shouldHideOnScroll maxWidth="xl">
+				<NavbarBrand>
+					<p className="font-bold text-inherit">Blockbase</p>
+				</NavbarBrand>
+				<NavbarContent className="hidden sm:flex gap-4" justify="center">
+					<NavbarItem>
+						<Link underline="hover" color="foreground" href="#">
+							About
+						</Link>
+					</NavbarItem>
+					<NavbarItem>
+						<Link underline="hover" color="foreground" href="#">
+							Customers
+						</Link>
+					</NavbarItem>
+					<NavbarItem>
+						<Dropdown>
+							<NavbarItem>
+								<DropdownTrigger>
+									<Button
+										disableRipple
+										className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+										endContent={icons.chevron}
+										radius="sm"
+										variant="light"
+									>
+										Live Chains
+									</Button>
+								</DropdownTrigger>
+							</NavbarItem>
+							<DropdownMenu
+								aria-label="Document Chain"
+								className="w-[340px]"
+								itemClasses={{
+									base: "gap-4",
+								}}
+							>
+								<DropdownItem
+									key="document"
+									description="Access documents stored on Live Chain"
+									startContent={icons.document}
+								>
+									Document Chain
+								</DropdownItem>
+								<DropdownItem
+									key="usage_metrics"
+									description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+									startContent={icons.activity}
+								>
+									Usage Metrics
+								</DropdownItem>
+								<DropdownItem
+									key="production_ready"
+									description="ACME runs on ACME, join us and others serving requests at web scale."
+									startContent={icons.flash}
+								>
+									Production Ready
+								</DropdownItem>
+								<DropdownItem
+									key="99_uptime"
+									description="Applications stay on the grid with high availability and high uptime guarantees."
+									startContent={icons.server}
+								>
+									+99% Uptime
+								</DropdownItem>
+								<DropdownItem
+									key="supreme_support"
+									description="Overcome any challenge with a supporting team ready to respond."
+									startContent={icons.user}
+								>
+									+Supreme Support
+								</DropdownItem>
+							</DropdownMenu>
+						</Dropdown>
+					</NavbarItem>
+				</NavbarContent>
+				<NavbarContent justify="end">
+					<NavbarItem>
+						<Button as={Link} color="primary" href="#" variant="flat">
+							Citizen Login
+						</Button>
+					</NavbarItem>
+					<NavbarItem>
+						<Button as={Link} color="primary" href="#" variant="flat">
+							Official Login
+						</Button>
+					</NavbarItem>
+				</NavbarContent>
+			</Navbar>
 		</motion.div>
-		
+
 	)
 }
